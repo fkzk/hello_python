a2e6f741870f97d7b9f83c542a6e6aeb7781d203
fukuzaki <fukuzaki@tkhm.elec.keio.ac.jp>
2022-09-03 11:51:46 +0900

    tuple, listのi番目の要素の読み書きを書いた

--- code.py
+++ code.py
@@ -1,9 +1,11 @@
 x_tuple = ('Hello', 123, 45.6, print) # tuple: ()で囲って列挙
-print(f'** {type(x_tuple)}の中身をfor文で展開 **')
-for x in x_tuple:
-    print(f'{x=}')
-
+print(f'{x_tuple=}')
 fruits_list = ['リンゴ', 'みかん', 'バナナ'] # list: []で囲って列挙
-print(f'** {type(fruits_list)}の中身をfor文で展開 **')
-for fruit in fruits_list:
-    print(f'{fruit}を食べた')
+print(f'{fruits_list=}')
+for i in (0, 2, -1):
+    print(f'** {i=}番目の要素を表示 **')
+    print(f'{x_tuple[i]=}')
+    print(f'{fruits_list[i]=}')
+fruits_list[0] = 'いちご' # listは要素を指定して代入できる
+print(f'{fruits_list=}') # 0番目（最初）の要素が 'リンゴ' → 'いちご' に
+x_tuple[0] = 'Good morning!' # tupleは要素に代入できないのでエラーが出る
