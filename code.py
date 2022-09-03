8b90905852c7b32831763d580208e06f0fe3d69f
fukuzaki <fukuzaki@tkhm.elec.keio.ac.jp>
2022-09-03 10:28:57 +0900

    変数の型を調べた

--- code.py
+++ code.py
@@ -1,12 +1,13 @@
 message = 'Good morning!'
 # "#"以降はプログラムに影響しないコメント（写しても写さなくてもOK）
 print(f'{message=}') # Python3.8～　変数名=値　の表示が可能（以降多用する）
+print(f'{type(message)=}') # type(変数名)で変数の型を調べられる
 number = 123
 print(f'{number=}')
+print(f'{type(number)=}')
 number = 45.6 # すでに使われている変数名の場合は上書きされる
 print(f'{number=}')
+print(f'{type(number)=}')
 print_fn = print # 関数も代入可能
 print(f'{print_fn=}')
-print_fn(f'{message=}')
-おまけ = '変数名は日本語でも大丈夫（使わないほうがいい）'
-print(f'{おまけ=}')
+print(f'{type(print_fn)=}')
