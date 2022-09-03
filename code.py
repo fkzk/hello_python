30dc389a49a760446c544d23c36668a978fa056d
fukuzaki <fukuzaki@tkhm.elec.keio.ac.jp>
2022-09-03 11:14:14 +0900

    繰り返しの対象をひとつの変数に代入した

--- code.py
+++ code.py
@@ -1,3 +1,5 @@
-for x in ('Good morning!', 123, 45.6, print):
+items = ('Good morning!', 123, 45.6, print) # 繰り返しの対象をひとつの変数に代入
+print(f'{items=}')
+print(f'{type(items)=}')
+for x in items:
     print(f'{x=}') # xには↑の()内の値が順次代入されていく
-    print(f'{type(x)=}')
