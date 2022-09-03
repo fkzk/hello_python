181aa16d74d81fb337b340eadd2187704248aa7d
fukuzaki <fukuzaki@tkhm.elec.keio.ac.jp>
2022-09-03 10:00:49 +0900

    文字列の中に変数を埋め込んで表示した

--- code.py
+++ code.py
@@ -1,2 +1,7 @@
 message = 'Good morning!'
 print(message)
+print(f'{message}')
+# "#"以降はプログラムに影響しないコメント（写しても写さなくてもOK）
+print(f'message') # {}で囲っていないのでそのまま"message"と表示される
+print(f'朝の挨拶：{message}') # 文字列の中に値が代入される
+print(f'{message=}') # Python3.8～　変数名=値　の表示が可能（以降多用する）
