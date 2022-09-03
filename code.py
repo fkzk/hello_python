23fd068943d5b4a6fa51da0b4a559d6b0bf12b62
fukuzaki <fukuzaki@tkhm.elec.keio.ac.jp>
2022-09-03 10:10:42 +0900

    変数に数値、関数を代入・再代入した

--- code.py
+++ code.py
@@ -1,7 +1,12 @@
 message = 'Good morning!'
-print(message)
-print(f'{message}')
 # "#"以降はプログラムに影響しないコメント（写しても写さなくてもOK）
-print(f'message') # {}で囲っていないのでそのまま"message"と表示される
-print(f'朝の挨拶：{message}') # 文字列の中に値が代入される
 print(f'{message=}') # Python3.8～　変数名=値　の表示が可能（以降多用する）
+number = 123
+print(f'{number=}')
+number = 45.6 # すでに使われている変数名の場合は上書きされる
+print(f'{number=}')
+print_fn = print # 関数も代入可能
+print(f'{print_fn=}')
+print_fn(f'{message=}')
+おまけ = '変数名は日本語でも大丈夫（使わないほうがいい）'
+print(f'{おまけ=}')
