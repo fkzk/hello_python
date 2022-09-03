b82a4c225f45d7ebcf26973fb224d7c3204596cd
fukuzaki <fukuzaki@tkhm.elec.keio.ac.jp>
2022-09-03 11:27:05 +0900

    enumerateで何回目のループかを表示した

--- code.py
+++ code.py
@@ -1,5 +1,3 @@
 items = ('Good morning!', 123, 45.6, print) # 繰り返しの対象をひとつの変数に代入
-print(f'{items=}')
-print(f'{type(items)=}')
-for x in items:
-    print(f'{x=}') # xには↑の()内の値が順次代入されていく
+for i, x in enumerate(items): # enumerateで囲うことにより何番目のループかわかる
+    print(f'{i}回目のループ {x=}') # iは0から始まる
