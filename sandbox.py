9730e08f1e9e2e1eddb62783022885d62af950c7
Fukuzaki Shota <fukuzaki@tkhm.elec.keio.ac.jp>
2022-10-26 15:20:55 +0900

    listとtupleの使い分けを学んだ

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,13 @@
 def main():
+    fruits = [ # a list of tuples
+        ('リンゴ', 'apple', 479),
+        ('みかん', 'orange', 339),
+        ('いちご', 'strawberry', 2064),
+        ('バナナ', 'banana', 185),
+    ]
+    # [演習] fruitsの中身を展開して「No. i: fruit」の形式でprint
+
+def check_types():
     # ( )の中を,区切りで列挙したものをtupleという
     # hello_world.pyで定義していたのは[ ]で囲ったlist
     item_tuple = (
