68828b7ff8fefee801b168c5840e7f2745921c3e
Fukuzaki Shota <fukuzaki@tkhm.elec.keio.ac.jp>
2022-10-26 14:14:24 +0900

    if文を使い、条件に当てはまるときだけの処理を書いた

--- hello_world.py
+++ hello_world.py
@@ -8,4 +8,6 @@ message_list = [
 ]
 # スライドでは個々より上の部分を次から省略
 for i_loop, item in enumerate(message_list):
+    if i_loop == 0: # == は”等しいかどうか"を判定
+        print('最初のループ')
     print(f'{i_loop}回目のループ: {item}')
