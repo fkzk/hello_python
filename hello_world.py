9a5beff2e7e08f54c8ea0b54b1a6ca33663e0c90
Fukuzaki Shota <fukuzaki@tkhm.elec.keio.ac.jp>
2022-10-26 14:26:55 +0900

    関数の引数の初期値を定義・指定した

--- hello_world.py
+++ hello_world.py
@@ -8,12 +8,12 @@ message_list = [
 ]
 # スライドでは個々より上の部分を次から省略
 
-def is_small(num, th): # define（定義する）のdef
+def is_small(num, th=3): # define（定義する）のdef
     return num < th
 
 for i_loop, item in enumerate(message_list):
-    if i_loop == 0: # == は”等しいかどうか"を判定
-        print('最初のループ') # これまで使ってきたprintも関数
-    elif is_small(i_loop, 3): # 小さいかどうかを判定していることがわかりやすい
-        print('もう少しがんばろう') # 0回目では実行されない
+    if is_small(i_loop, th=1):
+        print('最初のループ', end= ' / ') # print関数にも引数がある
+    elif is_small(i_loop): # thは省略可能（3として扱う）
+        print('もう少しがんばろう', end=' / ') # endの初期値は改行コード
     print(f'{i_loop}回目のループ: {item}')
