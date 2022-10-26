dc52f9de6d89522d5585a17461187390c62601a3
Fukuzaki Shota <fukuzaki@tkhm.elec.keio.ac.jp>
2022-10-26 15:14:03 +0900

    tupleにいろいろなものを列挙した

--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,13 @@
 def main():
-    print('メインの処理')
+    # ( )の中を,区切りで列挙したものをtupleという
+    # hello_world.pyで定義していたのは[ ]で囲ったlist
+    item_tuple = (
+        1, 1.0, "1.0", True, False, None, print
+    )
+    print(f'{type(item_tuple) = }')
+    print(f'{item_tuple = }')
+    print('')
+    # [演習]以下にtupleの中身それぞれについて上と同様に型・値を表示するコードを書く
 
 if __name__ == '__main__': # sandbox.pyを直接実行したら
     main()
