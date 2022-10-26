cc712fe73dcc002525065e52b3b81f987d9e34f6
Fukuzaki Shota <fukuzaki@tkhm.elec.keio.ac.jp>
2022-10-26 14:20:20 +0900

    関数を定義した

--- hello_world.py
+++ hello_world.py
@@ -7,9 +7,13 @@ message_list = [
     f'{message = }', # 実は最後の要素に,をつけてもOK
 ]
 # スライドでは個々より上の部分を次から省略
+
+def is_small(num): # define（定義する）のdef
+    return num < 3
+
 for i_loop, item in enumerate(message_list):
     if i_loop == 0: # == は”等しいかどうか"を判定
-        print('最初のループ')
-    elif i_loop < 3:
+        print('最初のループ') # これまで使ってきたprintも関数
+    elif is_small(i_loop): # 小さいかどうかを判定していることがわかりやすい
         print('もう少しがんばろう') # 0回目では実行されない
     print(f'{i_loop}回目のループ: {item}')
