11e25a8a676f65a57e19b59318033d8b4c85bb15
Fukuzaki Shota <fukuzaki@tkhm.elec.keio.ac.jp>
2022-10-26 14:09:27 +0900

    リストの中身を改行して見やすくした

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,11 @@
 message = 'Hello, World!'
-message_list = ['message', message, f'{message}', f'メッセージ: {message}', f'{message = }']
+message_list = [
+    'message',
+    message,
+    f'{message}',
+    f'メッセージ: {message}',
+    f'{message = }'
+]
 for item in message_list:
     print(item)
 print('繰り返し終了')
